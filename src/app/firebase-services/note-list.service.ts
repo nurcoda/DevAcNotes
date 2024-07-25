6741f7102d987bf2944b9f20435684bf5dfaa46d
nurcoda <mailo.mittelaedt@googlemail.com>
2024-07-25 17:14:07 +0200

    2 push

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { Firestore, collection, doc, collectionData } from '@angular/fire/firestore';
+import { Firestore, collection, doc, collectionData, onSnapshot } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { Note } from '../interfaces/note.interface';
 
@@ -11,11 +11,33 @@ export class NoteListService {
   trashNotes: Note[] = [];
   normalNotes: Note[] = [];
 
+  unsubList;
+  unsubSingle;
+
   items$;
+  items;
+
   firestore: Firestore = inject(Firestore);
 
   constructor() {
+    this.unsubList = onSnapshot(this.getNotesRef(), (list) => {
+      list.forEach(element => {
+        console.log(element);
+      });
+    });
+
+    this.unsubSingle = onSnapshot(this.getsingleDocRef("notes", "ji2r0trkkiC4KVgMr5Yr"), (element) => {
+    });
+
+    this.unsubSingle();
+
     this.items$ = collectionData(this.getNotesRef());
+    this.items = this.items$.subscribe((list) => {
+      list.forEach(element => {
+        console.log(element);
+      });
+    })
+    this.items.unsubscribe();
   }
 
   getNotesRef() {
